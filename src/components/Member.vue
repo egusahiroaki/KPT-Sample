836ec5c588524a89ec93cdcfd9b2b58af6be0f34
egusahiroaki <egusa.hiroaki@gmail.com>
2017-10-21 15:12:28 +0900

    fix

--- src/components/Member.vue
+++ src/components/Member.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-      <ul v-for='(elm, index) in allMembers'>
+      <ul v-for='(elm, index) in people'>
         <li>
+          {{elm.name}}
+<!-- 
           <div v-if="!elm.edit" class="display" v-text="elm.name" @click="elm.edit = true"></div>
           <input v-if="elm.edit" type="text" v-model="elm.name" ref="textInput" @keyup.enter="submit(elm)" />
+ -->
         </li>
       </ul>
 
     <div>
-      <el-button>Primary Button</el-button>
+      <el-button @click="submit">Primary Button</el-button>
     </div>
 
   </div>
@@ -24,8 +27,16 @@ export default {
       members: [] // {id: 0, name: 'test user', edit: false},
     }
   },
+    // membersAddedEditState () {
+    //   console.log('aaa')
+    //   this.getAllPeople.forEach((member) => {
+    //     member.edit = false
+    //     this.members.push(member)
+    //   })
+    //   return this.members
+    // }
   computed: mapGetters({
-    allMembers: 'getAllPeople'
+    people: 'getAllPeople'
   }),
   methods: {
     submit (member) {
