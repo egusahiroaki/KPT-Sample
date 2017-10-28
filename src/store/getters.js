d09594e8152be2f20c1f7267d4f42b97a1aabe85
egusahiroaki <egusa.hiroaki@gmail.com>
2017-10-29 04:01:57 +0900

    read api

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,4 +1,10 @@
+export const initailState = state => state
+
 export const getDashboard = state => {
+  console.log('getdashstate')
+  console.log(state.dashboard)
+  console.log(state.dashboard[0])
+
   const keepBoardItems = state.dashboard[0].items.map((item) => {
     if (item.userId === 0) {
       item.user = state.user
@@ -36,6 +42,9 @@ export const getDashboard = state => {
   dashboardReplacedByUser.push({title: 'KEEP', items: keepBoardItems})
   dashboardReplacedByUser.push({title: 'PROBLEM', items: problemBoardItems})
   dashboardReplacedByUser.push({title: 'TRY', items: tryBoardItems})
+
+  console.log('dashboardReplacedByUser')
+  console.log(dashboardReplacedByUser)
   return dashboardReplacedByUser
 }
 
@@ -43,6 +52,8 @@ export const getAllPeople = state => {
   let all = []
   all.push(state.user)
   state.members.forEach((elm) => all.push(elm))
+  console.log('all')
+  console.log(all)
   return all
 }
 
